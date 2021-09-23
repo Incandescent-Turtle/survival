45a83905bcd71fc4e046d9e1a6fa7d363839fd90
Incandescent-Turtle <59327500+Incandescent-Turtle@users.noreply.github.com>
2021-09-22 19:12:51 -0700

    Delete src/game/thesurvival/main directory

--- src/game/thesurvival/main/interfaces/Controllable.java
+++
@@ -1,8 +0,0 @@
-package game.thesurvival.main.interfaces;
-
-//for GameObjects that need controls passed to them
-public interface Controllable 
-{
-	public abstract void keyPressed(final int key);
-	public abstract void keyReleased(final int key);
-}
